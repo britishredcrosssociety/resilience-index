070e94d6917b363236ef84a5f1cd7859d32dcf32
Matt Thomas <matthewgthomas@gmail.com>
2020-11-16 17:20:24 +0000

    Added fire & rescue response times to capacity indicators

--- create domain - capacity.R
+++ create domain - capacity.R
@@ -2,6 +2,7 @@
 ## Create an overall 'capacity index'
 ## - LA spending power
 ## - VCS volunteer capacity
+## - Fire and Rescue response times
 ##
 library(tidyverse)
 
@@ -10,6 +11,7 @@ source("functions.R")
 # ---- Load data ----
 spending <- read_csv("data/processed/LA spending power.csv")
 volunteer <- read_csv("data/processed/volunteer scores.csv")
+fire_response <- read_csv("data/processed/fire and rescue response times.csv")
 
 # ---- Create capacity index -----
 # Build volunteer domain
@@ -23,10 +25,19 @@ volunteer <-
   ) %>%
   mutate(`Volunteer rank` = rank(`Volunteer score`))
 
+# Build emergency response domain
+fire_response <-
+  fire_response %>%
+  filter(str_detect(LAD19CD, "^E")) %>% 
+  select(LAD19CD, `Fire and Rescue response time (three-year average)`) %>%
+  mutate(`Emergency response score` = standardised(rank2(`Fire and Rescue response time (three-year average)`))) %>%
+  mutate(`Emergency response rank` = rank(`Emergency response score`))
+
 # Join domains and calculate capacity index
 capacity_index <-
   spending %>%
   left_join(volunteer, by = "LAD19CD") %>%
+  left_join(fire_response, by = "LAD19CD") %>%
   select(
     LAD19CD,
     ends_with("rank")
