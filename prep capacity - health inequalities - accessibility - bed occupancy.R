6225a6cdfe7526508732390a78709ecea684d42c
Matt Thomas <matthewgthomas@gmail.com>
2020-12-01 17:41:27 +0000

    Update prep capacity - health inequalities - accessibility - bed occupancy.R

--- prep capacity - health inequalities - accessibility - bed occupancy.R
+++ prep capacity - health inequalities - accessibility - bed occupancy.R
@@ -7,42 +7,7 @@ library(lubridate)
 library(readxl)
 library(httr)
 
-# ---- Load data ----
-# List of NHS hospitals in England
-hosp_eng <- read_csv("data/raw/health/england-hospitals.csv")
-
-lookup_trusts <- read_csv("data/raw/health/lookup old and new NHS Trust names.csv")
-
-# ---- Get Local Authorities for each hospital ----
-# Load postcode lookup to LAs
-# download and unzip Postcode to Output Area to Lower Layer Super Output Area to Middle Layer Super Output Area to Local Authority District (May 2020) Lookup in the UK
-# source: https://geoportal.statistics.gov.uk/datasets/postcode-to-output-area-to-lower-layer-super-output-area-to-middle-layer-super-output-area-to-local-authority-district-may-2020-lookup-in-the-uk
-GET("https://www.arcgis.com/sharing/rest/content/items/a8d42df48f374a52907fe7d4f804a662/data",
-    write_disk(tf <- tempfile(fileext = ".zip")))
-
-unzip(tf, exdir = "data/raw/postcodes")
-unlink(tf); rm(tf)
-
-# load postcode directory and keep only relevant fields - we just need MSOA
-postcodes_raw <- read_csv("data/raw/postcodes/PCD_OA_LSOA_MSOA_LAD_MAY20_UK_LU.csv")
-postcodes <- postcodes_raw %>% dplyr::select(Postcode = pcd7, ladcd)
-
-# the ONS data truncates 7-character postcodes to remove spaces (e.g. CM99 1AB --> CM991AB); get rid of all spaces in both datasets to allow merging
-# Mutate to upper case
-postcodes <- postcodes %>%
-  mutate(Postcode2 = str_to_upper(Postcode),
-         Postcode2 = str_replace_all(Postcode, " ", ""))
-
-hosp_eng <- hosp_eng %>%
-  mutate(Postcode2 = str_to_upper(Postcode),
-         Postcode2 = str_replace_all(Postcode, " ", ""))
-
-# Keep list of Trusts and the LAs they cover
-trust_lad <- hosp_eng %>% 
-  select(Name, Trust, Postcode2) %>% 
-  left_join(postcodes, by = "Postcode2") %>% 
-  select(Trust, LAD19CD = ladcd) %>% 
-  distinct()
+trust_lad <- read_csv("data/lookup-nhs-trust-to-la.csv")
 
 # ---- Download and process bed occupancy data for 2020-21 ----
 # Quarter 2
