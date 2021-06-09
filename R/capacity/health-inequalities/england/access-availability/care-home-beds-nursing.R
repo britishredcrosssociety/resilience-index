fbd027432f39ff032f72128f7d659b6bbaad4b68
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-06-09 14:53:32 +0200

    Add care home beds with  nursing data

---
+++ R/capacity/health-inequalities/england/access-availability/care-home-beds-nursing.R
@@ -0,0 +1,84 @@
+library(dplyr)
+library(tidyr)
+library(httr)
+library(readODS)
+library(geographr)
+library(sf)
+library(stringr)
+
+source("R/utils.R")
+
+# Source: https://www.cqc.org.uk/about-us/transparency/using-cqc-data#directory
+# Care directory with filters
+GET(
+  "https://www.cqc.org.uk/sites/default/files/1%20June%202021%20HSCA%20Active%20Locations.ods",
+  write_disk(tf <- tempfile(fileext = ".ods"))
+)
+
+care_homes_raw <- read_ods(tf, sheet = 2)
+
+# Remove vars
+care_homes <-
+  care_homes_raw %>%
+  select(
+    care_home = `Care home?`,
+    dormant = `Dormant (Y/N)`,
+    num_beds = `Care homes beds`,
+    postcode = `Location Postal Code`,
+    nursing = `Service type - Care home service with nursing`
+  )
+
+# Clean postcodes
+care_homes_postcodes <-
+  care_homes %>%
+  mutate(postcode = str_to_upper(postcode)) %>%
+  mutate(postcode = str_remove_all(postcode, " "))
+
+# Lookup postcode to LAD
+care_homes_joined <-
+  care_homes_postcodes %>%
+  left_join(
+    lookup_postcode_lad,
+    by = "postcode"
+  )
+
+# Filter to homes with nursing
+care_homes_nursing <-
+  care_homes_joined %>%
+  filter(nursing == "Y")
+
+# Count beds per LAD
+care_home_nursing_beds <-
+  care_homes_nursing %>%
+  filter(care_home == "Y") %>%
+  filter(dormant == "N") %>%
+  group_by(lad_code) %>%
+  mutate(num_beds = as.double(num_beds)) %>%
+  summarise(num_beds = sum(num_beds, na.rm = TRUE))
+
+# Drop care home beds which couldn't be matched (less than 0.09%)
+care_home_nursing_beds <-
+  care_home_nursing_beds %>%
+  drop_na()
+
+# Normalise per 1,000 population above 65
+pop_over_65 <-
+  population_lad %>%
+  select(lad_code, `65`:`90+`) %>%
+  pivot_longer(cols = !lad_code, names_to = "age", values_to = "count") %>%
+  group_by(lad_code) %>%
+  summarise(pop_over_65 = sum(count)) %>%
+  mutate(pop_over_65_per_1000 = pop_over_65 / 1000)
+
+care_home_nursing_beds_normalised <-
+  care_home_nursing_beds %>%
+  left_join(pop_over_65, by = "lad_code") %>%
+  mutate(nursing_beds_per_1000_over_65 = num_beds / pop_over_65_per_1000) %>%
+  select(lad_code, nursing_beds_per_1000_over_65)
+
+# Save
+care_home_nursing_beds_normalised %>%
+  write_rds("data/capacity/health-inequalities/england/care-home-beds-nursing.rds")
+
+# The LADs "E09000001" (City of London) and "E06000053" (Isles of Scilly) do not
+# appear to have any nursing beds. How should this "missing" data be handled?
--- R/capacity/health-inequalities/england/metadata.md
+++ R/capacity/health-inequalities/england/metadata.md
@@ -2,4 +2,7 @@
 
 | Domain | Indicator | Year | Source | License | Indicator Code Added to `R/` | Data Added to `data/` |
 | --- | --- | --- | --- | --- | --- | --- |
-| Access & Availability | Care Home Beds | 2021 | [CQC](https://www.cqc.org.uk/about-us/transparency/using-cqc-data#directory) | [OGL3](https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/) | :heavy_check_mark: | :heavy_check_mark: - there are no beds in the City of London "E09000001" |
+| Access & Availability | Care Home Beds | 2021 | [CQC](https://www.cqc.org.uk/about-us/transparency/using-cqc-data#directory) | [OGL3](https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/) | :heavy_check_mark: | :heavy_check_mark: - there are no beds in "E09000001" (City of London) |
+| Domain | Indicator | Year | Source | License | Indicator Code Added to `R/` | Data Added to `data/` |
+| --- | --- | --- | --- | --- | --- | --- |
+| Access & Availability | Care Home Beds with Nursing | 2021 | [CQC](https://www.cqc.org.uk/about-us/transparency/using-cqc-data#directory) | [OGL3](https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/) | :heavy_check_mark: | :heavy_check_mark: - there are no beds in "E09000001" (City of London) & "E06000053" (Isles of Scilly) |
