c884adf877b32c721796188d4229afa8937e7476
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-07-23 13:58:15 +0200

    Add life satisfaction data

---
+++ R/vulnerability/health-inequalities/wales/healthy-people/life-satisfaction.R
@@ -0,0 +1,39 @@
+# ---- Load ----
+library(tidyverse)
+library(httr)
+library(readxl)
+library(geographr)
+library(sf)
+
+source("R/utils.R")
+
+wales_lookup <-
+  boundaries_lad %>%
+  as_tibble() %>%
+  select(starts_with("lad")) %>%
+  filter_codes(lad_code, "^W")
+
+# ---- Extract data ----
+# Source: https://www.ons.gov.uk/datasets/wellbeing-local-authority/editions/time-series/versions/1
+GET(
+  "https://download.ons.gov.uk/downloads/datasets/wellbeing-local-authority/editions/time-series/versions/1.xlsx",
+  write_disk(tf <- tempfile(fileext = ".xlsx"))
+)
+
+life_satisfaction_raw <-
+  read_excel(tf, sheet = "Dataset", skip = 2)
+
+# The 'Average (mean)' estimate provides the score out of 0-10. The other estimates are
+# thresholds (percentages) described in the QMI: https://www.ons.gov.uk/peoplepopulationandcommunity/wellbeing/methodologies/personalwellbeingintheukqmi
+life_satisfaction <-
+  life_satisfaction_raw %>%
+  filter(Estimate == "Average (mean)") %>%
+  filter(MeasureOfWellbeing == "Life Satisfaction") %>%
+  select(
+    lad_code = `Geography code`,
+    life_satisfaction_score_out_of_10 = `2019-20`
+  ) %>%
+  right_join(wales_lookup) %>%
+  select(-lad_name)
+
+write_rds(life_satisfaction, "data/vulnerability/health-inequalities/wales/healthy-people/life-satisfaction.rds")
