c6aad06bf9f9ae0429e5a4506c748909c83aa944
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-10-15 11:34:43 +0200

    Calculate lad rates

--- R/capacity/health-inequalities/england/access-availability/waiting-lists.R
+++ R/capacity/health-inequalities/england/access-availability/waiting-lists.R
@@ -8,7 +8,7 @@ library(geographr)
 source("R/utils.R")
 
 msoa_pop <-
-  population_msoa |> 
+  population_msoa |>
   select(msoa_code, total_population)
 
 # Create trust lookup of open trusts
@@ -58,15 +58,15 @@ diagnostic_open_trusts <-
   left_join(
     diagnostics_vars,
     by = "Trust Code"
-  ) |> 
+  ) |>
   select(
     trust_code = `Trust Code`,
     waiting_13_weeks = `Waiting 13+ weeks`
   )
 
 # Remove NA values
-diagnostic_drop_na <- 
-  diagnostic_open_trusts |> 
+diagnostic_drop_na <-
+  diagnostic_open_trusts |>
   drop_na()
 
 # Aggregate to MSOA
@@ -74,48 +74,52 @@ diagnostic_drop_na <-
 # Notice, that many trusts don't have lookup data available. This is because
 # the catchment population data used in the trust to msoa lookup does not
 # include these trusts. This is presumably because the catchment data (2018) is
-# out of date with the Trust data. This needs verifying somehow. Note: the 
-# lookup script in geographr does attempt to update codes where a change has 
+# out of date with the Trust data. This needs verifying somehow. Note: the
+# lookup script in geographr does attempt to update codes where a change has
 # been made.
 # not_available <-
-#   diagnostic_drop_na |> 
+#   diagnostic_drop_na |>
 #   left_join(lookup_trust_msoa) |>
-#   keep_na() |> 
-#   pull(trust_code)  |> 
+#   keep_na() |>
+#   pull(trust_code)  |>
 #   unique()
 
-# points_nhs_trusts |> 
-#   filter(nhs_trust_code %in% not_avilable) |> 
+# points_nhs_trusts |>
+#   filter(nhs_trust_code %in% not_avilable) |>
 #   ggplot() +
 #   geom_sf(data = boundaries_lad) +
 #   geom_sf(size = 3) +
 #   theme_minimal()
 
 # Calculate MSOA proportions
-diagnostic_msoa <- 
-  diagnostic_drop_na |> 
-  left_join(lookup_trust_msoa) |> 
-  mutate(weighted = waiting_13_weeks * proportion) |> 
-  group_by(msoa_code) |> 
-  mutate(waiting_diagnostic = sum(weighted)) |> 
-  ungroup() |> 
-  select(msoa_code, waiting_diagnostic) |> 
+diagnostic_msoa <-
+  diagnostic_drop_na |>
+  left_join(lookup_trust_msoa) |>
+  drop_na() |> # Because the Trust lookup table is currently incomplete
+  mutate(weighted = waiting_13_weeks * proportion) |>
+  group_by(msoa_code) |>
+  mutate(waiting_diagnostic = sum(weighted)) |>
+  ungroup() |>
+  select(msoa_code, waiting_diagnostic) |>
   distinct()
 
 # Normalise by population
 diagnostic_msoa_normalised <-
-  diagnostic_msoa |> 
-  left_join(msoa_pop) |> 
-  mutate(diagnostic_rate = waiting_diagnostic/total_population * 100) |> 
+  diagnostic_msoa |>
+  left_join(msoa_pop) |>
+  mutate(diagnostic_rate = waiting_diagnostic / total_population * 100) |>
   select(msoa_code, diagnostic_rate, total_population)
 
-# diagnostic_lad <-
-diagnostic_msoa_normalised |> 
-  left_join(lookup_msoa_lad) |> 
-  keep_na()
+# Aggregate to LAD
+diagnostic_lad <-
+  diagnostic_msoa_normalised |>
+  left_join(lookup_msoa_lad) |>
+  calculate_extent(
+    var = diagnostic_rate,
+    higher_level_geography = lad_code,
+    population = total_population
+  )
 
 # TODO:
-# - find missing msoa in msoa_lad lookup
-# - calculate extent
-# - the missing Trusts need amending otherwise the data is likely going to be
+# - The missing Trusts need amending otherwise the data is likely going to be
 #   skewed/inaccurate
