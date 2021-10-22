5334db7e9e93d83f074a0fd80621cf7e0a780dac
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-10-22 16:52:27 +0200

    Initiate script

---
+++ R/capacity/health-inequalities/england/workforce/nhs-workforce.R
@@ -0,0 +1,4 @@
+# TODO:
+# 1. Inspect different data sets and choose most appropriate
+# 2. Make sure locum/agency spend is separated and not captured in the data to
+#    differentiate the two types of care.
