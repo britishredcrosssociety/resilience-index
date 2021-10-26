2a9c838b95eae853ab54494daef5877c2c51190b
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-10-26 10:47:46 +0200

    Add download file fun

--- R/utils.R
+++ R/utils.R
@@ -34,6 +34,32 @@ filter_codes <-
       )
   }
 
+#' Download a file temporarily to disk
+#' 
+#' @param url A URL for the request
+#' @param file_extension A character vector detailing the file extension (e.g.,
+#'         ".xlsx")
+download_file <-
+  function(url, file_extension) {
+
+    stopifnot(
+      !missing(url),
+      !missing(file_extension)),
+      is.character(url),
+      is.character(file_extension)
+      )
+
+    temp_path <-
+      tempfile(fileext = file_extension)
+
+    httr2::request(url) |>
+      httr2::req_perform(
+        path = temp_path
+      )
+
+    return(temp_path)
+  }
+
 #' Rank indicators with NAs first (i.e. 1 = worst)
 #'
 #' @param x Data to rank
