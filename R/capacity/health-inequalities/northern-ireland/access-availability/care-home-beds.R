15c40039ebf9254064a47c6840f2b19ac9b343e8
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-10-27 10:44:31 +0200

    Update data to use over 65 pop estimates

--- R/capacity/health-inequalities/northern-ireland/access-availability/care-home-beds.R
+++ R/capacity/health-inequalities/northern-ireland/access-availability/care-home-beds.R
@@ -28,10 +28,24 @@ care_home_beds_unweighted <-
   ) |>
   mutate(bed_count = as.double(bed_count))
 
+# Normalise per 1,000 population over 65
+pop_over_65 <-
+  population_trusts_ni |>
+  select(starts_with("trust_"), `65`:`90`) |>
+  pivot_longer(
+    cols = !starts_with("trust_"),
+    names_to = "age",
+    values_to = "count"
+  ) |>
+  group_by(trust_code, trust_name) |>
+  summarise(pop_over_65 = sum(count)) |>
+  ungroup()
+
+
 care_home_beds <-
   care_home_beds_unweighted |>
-  left_join(population_trusts_ni) |>
-  mutate(care_home_beds_per_1000 = bed_count / population * 1000) |>
+  left_join(pop_over_65) |>
+  mutate(care_home_beds_per_1000 = bed_count / pop_over_65 * 1000) |>
   select(trust_code, care_home_beds_per_1000)
 
 care_home_beds |>
