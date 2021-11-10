55a61eee0c2e21dabc0b58e63a19d581a30a8ff8
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-11-10 12:32:45 +0100

    add capacity domain scores

--- R/capacity/health-inequalities/northern-ireland/build-index.R
+++ R/capacity/health-inequalities/northern-ireland/build-index.R
@@ -34,7 +34,7 @@ access_availability_indicators <-
 
 # 1. Scale (align) indicators - Higher value = Higher capacity.
 access_availability_scaled <-
-  access_availability_indicators %>%
+  access_availability_indicators |>
   mutate(
     ambulance_response_time_seconds = ambulance_response_time_seconds * -1,
     outpatient_waiting_more_52_weeks_percent = outpatient_waiting_more_52_weeks_percent * -1
@@ -42,12 +42,12 @@ access_availability_scaled <-
 
 # 3. Weight the indicators within the domain
 access_availability_weighted <-
-  access_availability_scaled %>%
+  access_availability_scaled |>
   normalise_indicators()
 
 # 5. Calculate domain scores
 access_availability_scores <-
-  access_availability_weighted %>%
+  access_availability_weighted |>
   calculate_domain_scores(
     domain_name = "access_availability",
     num_quantiles = 5
@@ -63,20 +63,94 @@ workforce_indicators <-
 
 # 1. Scale (align) indicators - Higher value = Higher capacity.
 workforce_scaled <-
-  workforce_indicators %>%
+  workforce_indicators |>
   mutate(
     cancelled_operations_per_1000 = cancelled_operations_per_1000 * -1
   )
 
 # 3. Weight the indicators within the domain
 workforce_weighted <-
-  workforce_scaled %>%
+  workforce_scaled |>
   normalise_indicators()
 
 # 5. Calculate domain scores
 workforce_scores <-
-  workforce_weighted %>%
+  workforce_weighted |>
   calculate_domain_scores(
     domain_name = "workforce",
     num_quantiles = 5
   )
+
+# ---- Build Quality Domain ----
+# Load indicators
+quality_indicators <-
+  load_indicators(
+    path = "data/capacity/health-inequalities/northern-ireland/quality",
+    key = "trust_code"
+  )
+
+# 1. Scale (align) indicators - Higher value = Higher capacity.
+# Nothin to algin
+
+# 3. Weight the indicators within the domain
+quality_weighted <-
+  quality_indicators |>
+  normalise_indicators()
+
+# 5. Calculate domain scores
+quality_scores <-
+  quality_weighted |>
+  calculate_domain_scores(
+    domain_name = "quality",
+    num_quantiles = 5
+  )
+
+# ---- Save Domain Quantiles ----
+capacity_domain_scores <-
+  access_availability_scores |>
+  left_join(
+    workforce_scores,
+    by = "trust_code"
+  ) |>
+  left_join(
+    quality_scores,
+    by = "trust_code"
+  ) |>
+  select(
+    trust_code,
+    ends_with("_quantiles")
+  )
+
+capacity_domain_scores |>
+  write_csv("data/capacity/health-inequalities/northern-ireland/capacity-domain-scores.csv")
+
+# ---- Combine Domains ----
+# 6. Combine domains with equal weighting to produce composite score
+# capacity_scores <-
+#   access_availability_scores |>
+#   left_join(
+#     workforce_scores,
+#     by = "trust_code"
+#   ) |>
+#   left_join(
+#     quality_scores,
+#     by = "trust_code"
+#   ) |>
+#   select(
+#     trust_code,
+#     ends_with("domain_score")
+#   ) |>
+#   rowwise(!where(is.numeric)) |>
+#   summarise(composite_score = sum(c_across(where(is.numeric)))) |>
+#   ungroup() |>
+#   mutate(composite_rank = rank(composite_score)) |>
+#   mutate(composite_quantiles = quantise(composite_rank, 5)) |>
+#   rename_with(
+#     ~ str_c("hi_capacity", .x, sep = "_"),
+#     where(is.numeric)
+#   )
+
+# TODO: The calculate_composite_scores() function has had to be adpated to get
+# ditinct ranks, but this has places a much higher emphasis on the access and
+# availability domain which has much higher normalised domain scores. What can
+# be done about this?
