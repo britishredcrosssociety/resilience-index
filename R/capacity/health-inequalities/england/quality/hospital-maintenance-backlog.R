b3dd636405190ee8994d0b720f28f9b9fb6e8f76
Aileen McDonald <aileen_mcdonald14@hotmail.com>
2021-12-10 10:20:01 +0000

    hospital maint backlog mapping trusts to msoa to la

--- R/capacity/health-inequalities/england/quality/hospital-maintenance-backlog.R
+++ R/capacity/health-inequalities/england/quality/hospital-maintenance-backlog.R
@@ -5,10 +5,10 @@ library(sf)
 library(readODS)
 
 source("R/utils.R") # for download_file()
+source("R/capacity/health-inequalities/england/trust_types/trust_types.R") # run trust types code
 
-
-# There was trust level data for 'Investment to reduce backlog maintenance (£)' but investment != cost and so have used the site level data for 
-# 'Cost to eradicate high risk backlog (£)' and aggregated the cost up to trust level 
+# There was trust level data for 'Investment to reduce backlog maintenance (£)' but investment != cost and so have used the site level data for
+# 'Cost to eradicate high risk backlog (£)' and aggregated the cost up to trust level
 # In case needed in future the trust level investment data can be found at https://files.digital.nhs.uk/84/07227E/ERIC%20-%20201920%20-%20TrustData.csv
 
 # NHS site level data on maintenance backlog -----
@@ -25,7 +25,7 @@ raw_site <-
 site_maintance_columns <- c("Cost to eradicate high risk backlog (£)", "Cost to eradicate significant risk backlog (£)", "Cost to eradicate moderate risk backlog (£)", "Cost to eradicate low risk backlog (£)")
 
 site_columns <- raw_site |>
-  select("Trust Code", "Trust Name", "Site Code", "Site Name", "Site Type", site_maintance_columns) |>
+  select("Trust Code", "Trust Name", "Site Code", "Site Name", "Site Type", all_of(site_maintance_columns)) |>
   mutate_at(site_maintance_columns, ~ ifelse(.x == "Not Applicable", NA, .x)) |>
   mutate_at(site_maintance_columns, ~ as.numeric(str_remove_all(.x, ",")))
 
@@ -44,14 +44,9 @@ site_agg_columns <- site_agg |>
 
 # NHS TRUST table in geographr package -----
 
-# Create trust lookup of open trusts
-open_trusts <-
-  points_nhs_trusts |>
-  as_tibble() |>
-  filter(status == "open") |>
-  select(
-    trust_code = nhs_trust_code
-  )
+# Load in open trusts table created in trust_types.R
+open_trusts <- arrow::read_feather("R/capacity/health-inequalities/england/trust_types/open_trust_types.feather")
+
 
 # Check the matching of cost data & trust table in geographr package --------
 
@@ -70,60 +65,58 @@ trusts_missing_maint_backlog <- high_risk_cost_open |>
 points_nhs_trusts |>
   as_tibble() |>
   filter(nhs_trust_code %in% trusts_missing_maint_backlog)
-# 4 trusts missing 
+# 4 trusts missing
 
 # Checked the https://github.com/britishredcrosssociety/geographr/blob/main/data-raw/lookup_trust_msoa.R for these
 # R0D used to be RD3 - which is in the cost data
 # RQF?
 # RT4 used to be RKV, RP8, RQE, RRC - which aren't in the cost data
-# RYT?     
+# RYT?
 
 # Check which trusts are in cost data and not geographr package --
 trusts_missing_geographr <- site_agg_columns |>
-  anti_join(open_trusts, by = c("Trust Code" = "trust_code")) 
+  anti_join(open_trusts, by = c("Trust Code" = "trust_code"))
 
 trusts_open_closed <- points_nhs_trusts |>
-  as_tibble() 
+  as_tibble()
 
 trusts_missing_geographr |>
-  left_join(trusts_open_closed,  by = c("Trust Code" = "nhs_trust_code"))
-# 9 are closed ones and then 4 remaining - TAD, TAF, TAH, TAJ
+  left_join(trusts_open_closed, by = c("Trust Code" = "nhs_trust_code"))
+# 5 are closed ones and then 10 remaining - TAD, TAF, TAH, TAJ, RA3, RBA, RC1, RDD, RQ6, RQ8
 
 # Join trust to MSOA lookup --------
 
-# Check if any trusts not in lookup table
-missing_trusts_from_lookup <- high_risk_cost_open |>
-  anti_join(lookup_trust_msoa) |>
-  distinct(trust_code) |>
-  print(n = Inf)
-
-missing_trusts_from_lookup |>
-  left_join(as_tibble(points_nhs_trusts), by = c("trust_code" = "nhs_trust_code")) |>
-  print(n = Inf)
-
-## Extra checks ##
-# Downloading CQC rating data as has information on what is the primary type of care trust provides ---
-# This is used to check against the trusts with no death data
-tf <- download_file("https://www.cqc.org.uk/sites/default/files/01_November_2021_Latest_ratings.ods", "ods")
-
-raw_providers <-
-  read_ods(
-    tf,
-    sheet = "Providers",
-  )
-
-trust_categories <- raw_providers |>
-  select(`Provider ID`, `Provider Name`, `Provider Type`, `Provider Primary Inspection Category`) |>
-  distinct()
-
+# Trust to MSOA table only has data for acute trusts
 high_risk_cost_open |>
-  anti_join(lookup_trust_msoa)  |>
-  left_join(trust_categories, by = c("trust_code" = "Provider ID")) |>
+  left_join(lookup_trust_msoa) |>
   group_by(`Provider Primary Inspection Category`) |>
-  summarise(count = n(), total_cost = sum(`Cost to eradicate high risk backlog (£)`, na.rm = T))
-# Vast majority of the missing trusts are ambulance, community health and mental health providers
+  summarise(count = n(), prop_with_lookup = sum(!is.na(msoa_code)) / n())
 
-# TODO:
-# Trusts need to be matched to MSOA's, and then aggregated up to LTLA.
-# As detailed in the 'access-availability/ae-waiting-times.R' script not all trusts are matching the
-# lookup table (68 not found in lookup table). This needs fixing before aggregation can happen.
+# Current approach is to drop information on non-acute trusts since can't proportion these to MSOA
+# For the acute trusts proportion these to MSOA and then aggregate to LSOA and proportion to per captia level
+
+high_risk_cost_msoa <- high_risk_cost_open |>
+  inner_join(lookup_trust_msoa) |>
+  mutate(cost_prop = `Cost to eradicate high risk backlog (£)` * proportion) |>
+  group_by(msoa_code) |>
+  summarise(cost_per_msoa = sum(cost_prop))
+
+high_risk_cost_lad <-
+  high_risk_cost_msoa |>
+  left_join(lookup_msoa_lad) |>
+  group_by(lad_code) |>
+  summarise(cost_per_lad = sum(cost_per_msoa))
+
+pop_lad <-
+  population_lad |>
+  select(lad_code, pop = total_population)
+
+high_risk_cost_lad_per_capita <-
+  high_risk_cost_lad |>
+  left_join(pop_lad) |>
+  mutate(cost_per_capita = cost_per_lad / pop * 100) |>
+  select(lad_code, cost_per_capita)
+
+# Save ----
+high_risk_cost_lad_per_capita |>
+  write_rds("data/capacity/health-inequalities/england/hospital_maintanance_backlog_cost.rds")
---
+++ R/capacity/health-inequalities/england/trust_types/trust_types.R
@@ -0,0 +1,33 @@
+# This script joins the trust type (from the CQC rating data) onto the open trust data is geographr package  
+
+# Load packages
+library(geographr)
+library(tidyverse)
+
+# Downloading CQC rating data as has information on what is the primary type of care trust provides ---
+# Landing page of data: https://www.cqc.org.uk/about-us/transparency/using-cqc-data#directory
+tf <- download_file("https://www.cqc.org.uk/sites/default/files/01_December_2021_Latest_ratings.ods", "ods")
+
+raw_providers <-
+  read_ods(
+    tf,
+    sheet = "Providers",
+  )
+
+providers <- raw_providers |>
+  distinct(`Provider ID`, `Provider Primary Inspection Category`) 
+
+# Joining onto open trusts geographr data
+
+open_trusts <-
+  geographr::points_nhs_trusts |>
+  as_tibble() |>
+  filter(status == "open") |>
+  select(
+    trust_code = nhs_trust_code
+  )
+
+open_trust_types <- open_trusts |>
+  left_join(providers, by = c("trust_code" = "Provider ID"))
+
+arrow::write_feather(open_trust_types, "open_trust_types.feather")
