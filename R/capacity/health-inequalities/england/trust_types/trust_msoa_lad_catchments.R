79f27cdb990c5b248a51cbf9efe416515db6c72e
Aileen McDonald <aileen_mcdonald14@hotmail.com>
2021-12-18 10:04:20 +0000

    update the trust to msoa/lad code

--- R/capacity/health-inequalities/england/trust_types/trust_msoa_lad_catchments.R
+++ R/capacity/health-inequalities/england/trust_types/trust_msoa_lad_catchments.R
@@ -1,25 +1,60 @@
 library(geographr)
 library(sf)
 library(tidyverse)
+library(httr)
+library(readxl)
 library(arrow)
 
-# 2019 MSOA pop
-msoa_pop <- geographr::population_msoa |>
-  select(msoa_code, total_population)
 
-lookup_trust_msoa_full <- geographr::lookup_trust_msoa |>
-  left_join(msoa_pop) |>
-  rename(msoa_pop = total_population) |>
-  mutate(msoa_catchment_for_trust = proportion * msoa_pop) |>
+# This is the data the geapgraphr::lookup_trust_msoa is on but for this need extra columns
+query_url <- "https://app.box.com/index.php?rm=box_download_shared_file&shared_name=qh8gzpzeo1firv1ezfxx2e6c4tgtrudl&file_id=f_877983829510"
+
+# Make GET request
+GET(
+  query_url,
+  write_disk(tf <- tempfile(fileext = ".xlsx"))
+)
+
+# All admissions
+catchment_populations <- read_excel(tf, sheet = "All Admissions")
+
+# Keep only 2019 data
+catchment_proportions <-
+  catchment_populations %>%
+  filter(CatchmentYear == 2019) %>%
+  select(
+    msoa_code = msoa,
+    trust_code = TrustCode,
+    patients,
+    total_patients,
+    proportion,
+    msoa_total_catchment,
+    trust_total_catchment
+  )
+
+
+# Although there are Trust catchments in the dataset, these are meant to be the sum of the msoa_catchment across each trusts but figures do not align. 
+catchment_proportions |> 
+  group_by(trust_code) |>
+  mutate(calculated_trust_total_catchment = sum(msoa_total_catchment))
+
+# Also note the total of the patietns column for an MSOA is not the same as the total_aptients column, is it that some patients for a given MSOA which can't be attributed to a Trust?
+# This means that the proportion column does not sum to 100% when grouped by Trust
+catchment_proportions |> 
+  select(msoa_code, patients, total_patients) |>
+  group_by(msoa_code) |>
+  mutate(calculated_total_patients = sum(patients))
+
+
+# Following similar logic to Victims of Maths using this data to attribute COVID deaths at Trust level to LAs
+# https://github.com/VictimOfMaths/COVID-19/blob/5803fa341b3af99e7a8d8b4eda708e6a3d2ab052/Heatmaps/COVIDAdmissionsLTLAPhasePlot.R#L73 
+lookup_trust_msoa_full <- catchment_proportions |>
+  select(msoa_code, trust_code, proportion, msoa_catchment_for_trust = msoa_total_catchment) |>
   group_by(trust_code) |>
   mutate(trust_catchment = sum(msoa_catchment_for_trust)) |>
   ungroup() |>
   rename(msoa_prop_by_trust = proportion) |>
   mutate(trust_prop_by_msoa = msoa_catchment_for_trust/trust_catchment)
-    
-# Note that not all MSOAs have a sum of 100% when total the column msoa_prop_by_trust. 
-# Some patients for a given MSOA which can't be attributed to a Trust?
-# The raw patient numbers for each MSOA/Trust combo is available in the workings of geographr package
 
 lookup_trust_lad <- lookup_trust_msoa_full |>
   left_join(lookup_msoa_lad) |>
