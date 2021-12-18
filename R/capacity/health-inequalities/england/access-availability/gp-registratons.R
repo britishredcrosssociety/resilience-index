3d961bc7cbf1c1bec12f512f3ad8610490979be2
Aileen McDonald <aileen_mcdonald14@hotmail.com>
2021-12-18 14:06:56 +0000

    try updated population data for gp regs & account for LAD restrcutures

--- R/capacity/health-inequalities/england/access-availability/gp-registratons.R
+++ R/capacity/health-inequalities/england/access-availability/gp-registratons.R
@@ -4,6 +4,7 @@ library(httr)
 library(readxl)
 library(geographr)
 library(sf)
+
 source("R/utils.R")
 
 # ---- Load data ----
@@ -56,25 +57,107 @@ lad_registrations <-
 gp_registrations <-
   lad_registrations |>
   left_join(pop_lad) |>
+  rename(pop = pop) |>
   mutate(perc_registered_gp = count / pop * 100) 
 
-
-summary(gp_registrations)
-
+# Check missings 
 gp_registrations |>
   filter(is.na(count) | is.na(pop))
+# 4 LADs with missing population data
 
-gp_registrations |>
-  drop_na() |>
+# Deal with LAD restructures -----
+
+# Some LADs restructured in 2020 & 2021 - since GP data and population data is at different years may need to use lookups (depending on what point in time data collected as so which LADs used)
+buckinghamshire_restructure_2020 <-
+  tribble(
+    ~pre_lad_name, ~post_ua_name, ~pre_lad_code, ~post_ua_code,
+    "Aylesbury Vale", "Buckinghamshire", "E07000004", "E06000060",
+    "Chiltern", "Buckinghamshire", "E07000005", "E06000060",
+    "South Bucks", "Buckinghamshire", "E07000006", "E06000060",
+    "Wycombe", "Buckinghamshire", "E07000007", "E06000060"
+  )
+
+northamptonshire_restructure_2021 <-
+  tribble(
+    ~pre_lad_name, ~post_ua_name, ~pre_lad_code, ~post_ua_code,
+    "Corby", "North Northamptonshire", "E07000150", "E06000061",
+    "Daventry", "West Northamptonshire", "E07000151", "E06000062",
+    "East Northamptonshire", "North Northamptonshire", "E07000152", "E06000061",
+    "Kettering", "North Northamptonshire", "E07000153", "E06000061",
+    "Northampton", "West Northamptonshire", "E07000154", "E06000062",
+    "South Northamptonshire", "West Northamptonshire", "E07000155", "E06000062",
+    "Wellingborough", "North Northamptonshire", "E07000156", "E06000061",
+  )
+
+gp_registrations_buck <- gp_registrations |>
+  filter(is.na(pop)) |>
+  left_join(buckinghamshire_restructure_2020, by = c("lad_code" = "pre_lad_code")) |>
+  group_by(post_ua_code) |>
+  summarise(count = sum(count)) |>
+  rename(lad_code = post_ua_code) |>
+  left_join(pop_lad) |>
+  mutate(perc_registered_gp = count / pop * 100) 
+
+gp_registrations_clean <- gp_registrations |>
+  filter(!is.na(pop)) |>
+  bind_rows(gp_registrations_buck)
+  
+# Check distribution
+summary(gp_registrations_clean$perc_registered_gp)
+
+# Total population data for England is less than the GP registrations
+gp_registrations_clean |>
   summarise(total_gp_reg = sum(count), total_pop = sum(pop))
 
+# May need to assume that the over registrations (perhaps by people that have left country or died) is constient across all LADs (as don't have info to test this) and use as relative comparison between LADs
+# ONS did some analysis in 2016 on the difference between GP regs and pop but found mix of over and under differences https://commonslibrary.parliament.uk/population-estimates-gp-registers-why-the-difference/
+# Not sure if can resister with more than 1 GP but perhaps is temporary regs? https://www.nhs.uk/common-health-questions/nhs-services-and-treatments/how-do-i-register-as-a-temporary-resident-with-a-gp/
 
-# TODO:
-# 1. There are four NA values in gp_registrations due to missing pop values.
-#    It is assumed these values are missing because 2021 lad codes have been
-#    used and so don't align. Check this assumption and align codes. See
-#    carers-allowance.R script for potential alignment solution.
-# 2. How do we handle the fact that only 10 LADs have less than 100% gp
-#    registration? The issue is that because we use deciles mainly, even places
-#    ranked as 100% registration will get lumped into the most vulnerable
-#    decile. This doesn't make sense.
+# Try the mid-2020 population figures -----
+tf <- download_file("https://www.ons.gov.uk/file?uri=%2fpeoplepopulationandcommunity%2fpopulationandmigration%2fpopulationestimates%2fdatasets%2fpopulationestimatesforukenglandandwalesscotlandandnorthernireland%2fmid2020/ukpopestimatesmid2020on2021geography.xls", "xlsx")
+
+pop_lad_2020 <-
+  read_excel(
+    tf,
+    sheet = "MYE2 - Persons",
+    skip = 7
+  )
+
+# Keep only valid 2020 LAD codes for England, Scotland, and Wales
+pop_lad_2020_clean <-  pop_lad_2020 %>%
+  select(lad_code = Code, lad_name = Name, geography = Geography, pop = `All ages`) |>
+  filter(str_detect(lad_code, "^E")) 
+
+gp_regs_pop_2020 <- gp_registrations |>
+  select(lad_code, count) |>
+  left_join(pop_lad_2020_clean)
+
+# Check missings
+gp_regs_pop_2020 |>
+  filter(is.na(pop)) 
+
+# Due to local authority restructuring (see line 68)
+combined_restructures <- buckinghamshire_restructure_2020 |>
+  bind_rows(northamptonshire_restructure_2021)
+
+gp_regs_pop_2020_restructures <- gp_regs_pop_2020 |>
+  filter(is.na(pop)) |>
+  left_join(combined_restructures, by = c("lad_code" = "pre_lad_code")) |>
+  group_by(post_ua_code) |>
+  summarise(count = sum(count)) |>
+  rename(lad_code = post_ua_code) |>
+  left_join(pop_lad_2020_clean) |>
+  mutate(perc_registered_gp = count / pop * 100) 
+  
+gp_regs_pop_2020_clean <- gp_regs_pop_2020 |>
+  filter(!is.na(pop)) |>
+  bind_rows(gp_regs_pop_2020_restructures)
+
+# Check distribution
+summary(gp_regs_pop_2020_clean$perc_registered_gp)
+
+# Total population data for England is less than the GP registrations
+gp_regs_pop_2020_clean |>
+  summarise(total_gp_reg = sum(count), total_pop = sum(pop))
+
+# Same issue with newer population data.
