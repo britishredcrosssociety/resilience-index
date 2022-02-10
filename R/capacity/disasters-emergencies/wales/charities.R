6eb6aac757fa5f22662ce2e00ddb27a099326a56
Ruonan Zhang <ruonan.1.zhang@kcl.ac.uk>
2022-02-10 13:08:41 +0000

    1. Modified housing-quality, access-to-service; 2. Delete imd.R in vulnerability/disater/wales; 3.Use another way to calculate charity income by LA

--- R/capacity/disasters-emergencies/wales/charities.R
+++ R/capacity/disasters-emergencies/wales/charities.R
@@ -6,6 +6,7 @@ source("R/utils.R")
 
 # ---- Load data ----
 # Source: https://register-of-charities.charitycommission.gov.uk/register/full-register-download
+
 # Chairty list
 GET(
   "https://ccewuksprdoneregsadata1.blob.core.windows.net/data/txt/publicextract.charity.zip",
@@ -22,46 +23,82 @@ charity_list_raw <-
     )
   )
 
+# Charity areas of operation
+GET(
+  "https://ccewuksprdoneregsadata1.blob.core.windows.net/data/txt/publicextract.charity_area_of_operation.zip",
+  write_disk(tf <- tempfile(fileext = ".zip"))
+)
+unzip(tf, exdir = tempdir())
+
+charity_areas_raw <-
+  read_tsv(
+    list.files(
+      tempdir(),
+      pattern = "publicextract.charity_area_of_operation.txt",
+      full.names = TRUE
+    )
+  )
+
 # Clean chairty list
-charity_list_clean <-
+charity_list_active <-
   charity_list_raw %>%
   filter(
     charity_registration_status == "Registered" &
     charity_insolvent == FALSE &
-    charity_in_administration == FALSE &
-    !is.na(charity_contact_postcode)
+    charity_in_administration == FALSE
   ) %>%
   select(
-      org_num = "organisation_number",
-      registed_num = "registered_charity_number",
-      charity_name = "charity_name",
-      status = "charity_registration_status",
-      postcode = "charity_contact_postcode"
-  ) %>%
-  as_tibble()
-
-# Remove the whitespace in postcode in order to join lookup_postcode_lad
-charity_list_clean$postcode  <-
-  str_replace_all(charity_list_clean$postcode, " ", "")
-
-# Join charity list with lookup_postcode_lad, filter Wales
-# then count charity numbers group by lad_code
-charity_lad <-
-  charity_list_clean %>%
-  left_join(lookup_postcode_lad, by = "postcode") %>%
-  filter(str_detect(lad_code, "^W")) %>%
-  count(lad_code) %>%
-  mutate(
-    deciles =
-      quantise(
-        n,
-        num_quantiles = 10,
-        highest_quantile_worst = FALSE
-      )
+      organisation_number = "organisation_number",
+      latest_income = "latest_income",
+      latest_expenditure = "latest_expenditure"
   )
 
-  
+# Clean charity area,
+# Only select Wales LA,
+# The result shows that one organisation could has multiple geo_area_description
+charity_areas_clean  <-
+  charity_areas_raw %>%
+  filter(
+    geographic_area_type == "Local Authority",
+    welsh_ind == TRUE
+  ) %>%
+  select(
+    organisation_number,
+    geographic_area_type,
+    lad_name = "geographic_area_description"
+  ) %>%
+  inner_join(charity_list_active, by = "organisation_number") 
+
+# Though one orgnisation had multiple geo_area_description, their latest income are the same
+# Question here is how to distinguish charity income in different LA?
+charity_areas_clean %>% 
+  filter(latest_income > 0)
+
+
+# Welsh Local Authorities
+charity_income_lad <-
+  charity_areas_clean %>%
+  mutate(
+    lad_name = case_when(
+      lad_name == "City Of Swansea" ~ "Swansea",
+      lad_name == "Rhondda Cynon Taff" ~ "Rhondda Cynon Taf",
+      lad_name == "Newport City" ~ "Newport",
+      TRUE ~ lad_name
+    ),
+    lad_name = tolower(lad_name)
+  ) %>%
+  left_join(
+    lookup_counties_ua_lad %>%
+      mutate(lad_name = tolower(lad_name)),
+    by = "lad_name"
+  ) %>%
+  mutate(
+    latest_income = if_else(is.na(latest_income), 0, latest_income) # replace NA by 0
+  ) %>%
+  group_by(lad_code) %>%
+  summarise(across(latest_income, sum)) # aggregate by la
+
 write_rds(
-  charity_lad,
+  charity_income_lad,
   "data/capacity/disasters-emergencies/wales/charity-lad.rds"
 )
