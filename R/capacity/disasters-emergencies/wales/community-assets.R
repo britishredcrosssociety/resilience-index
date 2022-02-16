2aa445fda0b9e7bfd2556cb6102435297134ce1f
Ruonan Zhang <ruonan.1.zhang@kcl.ac.uk>
2022-02-16 16:32:28 +0000

    use calculate_extent on community_assets

--- R/capacity/disasters-emergencies/wales/community-assets.R
+++ R/capacity/disasters-emergencies/wales/community-assets.R
@@ -1,18 +1,32 @@
 # load packages
 library(tidyverse)
 library(readxl)
+library(geographr)
 
 community_assets_raw <- read_excel("data/on-disk/community-needs-index/community-needs-index/WCNI and domains - 2021.xlsx")
 
 community_assets_lad <-
-    community_assets_raw %>%
-    select(
-        msoa_code = "MSOA code",
-        Civic_Assets_Domain = "Civic_Assets_Domain"
+  community_assets_raw %>%
+  select(
+    msoa_code = "MSOA code",
+    Civic_Assets_Domain = "Civic_Assets_Domain"
+  ) %>%
+  left_join(lookup_msoa_lad, by = "msoa_code") %>%
+  left_join(population_lad, by = "lad_code") %>%
+
+  calculate_extent(
+    var = Civic_Assets_Domain,
+    higher_level_geography = lad_code,
+    population = total_population,
+    invert_percentiles = FALSE
+  ) %>%
+
+  mutate(
+    rank = rank(extent),
+    deciles = quantise(rank, num_quantiles = 10)
     ) %>%
-    left_join(lookup_msoa_lad, by = "msoa_code") %>%
-    group_by(lad_code) %>%
-    summarise(mean(Civic_Assets_Domain)) # aggregation method need to consider
+  select(-extent, -rank)
+
 
 write_rds(
   community_assets_lad,
