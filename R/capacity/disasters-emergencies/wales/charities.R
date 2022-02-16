dca198a7b3eed5a37e5257f5601cc43f068eb8b9
Ruonan Zhang <ruonan.1.zhang@kcl.ac.uk>
2022-02-16 15:55:39 +0000

    use log tranform

--- R/capacity/disasters-emergencies/wales/charities.R
+++ R/capacity/disasters-emergencies/wales/charities.R
@@ -2,6 +2,7 @@
 library(httr)
 library(tidyverse)
 library(geographr)
+library(DataExplorer)
 source("R/utils.R")
 
 # ---- Load data ----
@@ -40,6 +41,7 @@ charity_areas_raw <-
   )
 
 # Clean chairty list
+# ??? Need to filter charity type?
 charity_list_active <-
   charity_list_raw %>%
   filter(
@@ -50,14 +52,24 @@ charity_list_active <-
   select(
       organisation_number = "organisation_number",
       latest_income = "latest_income",
-      latest_expenditure = "latest_expenditure"
+      latest_expenditure = "latest_expenditure",
+      charity_type = "charity_type"
   )
 
-# Clean charity area,
-# Only select Wales LA, this can filter some "BIG" charities which has high income
+
+# # Data exlporation
+# plot_intro(charity_list_active)
+# # 11% missing on latest_income and expenditure
+# plot_missing(charity_list_active)
+# # histogram chart
+# # !!! Few "BIG" charities have extremely large value, most are small, use log transform
+# plot_histogram(charity_list_active)
+
+# Clean charity area
 # The result shows that one organisation could has multiple geo_area_description
 charity_areas_clean  <-
   charity_areas_raw %>%
+  # Only select Wales LA
   filter(
     geographic_area_type == "Local Authority",
     welsh_ind == TRUE
@@ -67,17 +79,34 @@ charity_areas_clean  <-
     geographic_area_type,
     lad_name = "geographic_area_description"
   ) %>%
-  inner_join(charity_list_active, by = "organisation_number") 
+  inner_join(charity_list_active, by = "organisation_number")
 
-# Though one orgnisation had multiple geo_area_description, their latest income are the same
-# Question here is how to distinguish charity income in different LA?
-charity_areas_clean %>% 
+# To avoid skewness, try log transformation
+charity_areas_log <-
+  charity_areas_clean %>%
+  mutate(
+    latest_income = log(latest_income + 1),  # +1 to avoid -inf
+    latest_income = replace_na(latest_income, 0),  # replace NA with 0
+    latest_expenditure = log(latest_expenditure + 1),  # +1 to avoid -inf
+    latest_expenditure = replace_na(latest_expenditure, 0)  # replace NA with 0
+  )
+
+
+# plot_histogram(charity_areas_log)  # looks normal
+# # 0.01% missisng on charity type，
+# # 1.57% missisng on income and expenditure
+# plot_missing(charity_areas_log)
+
+
+# Though one orgnisation works in multiple lad, their latest income are the same
+# !!! Question here is how to distinguish charity income in different LA?
+charity_areas_log %>% 
   filter(latest_income > 0)
 
 
 # Welsh Local Authorities
 charity_income_lad <-
-  charity_areas_clean %>%
+  charity_areas_log %>%
   mutate(
     lad_name = case_when(
       lad_name == "City Of Swansea" ~ "Swansea",
@@ -91,9 +120,6 @@ charity_income_lad <-
     lookup_counties_ua_lad %>%
       mutate(lad_name = tolower(lad_name)),
     by = "lad_name"
-  ) %>%
-  mutate(
-    latest_income = if_else(is.na(latest_income), 0, latest_income) # replace NA by 0
   ) %>%
   group_by(lad_code) %>%
   summarise(across(latest_income, sum)) # aggregate by LA
