4f158cb2284e7f76444465df0e555ba0d62d6200
Ruonan Zhang <ruonan.1.zhang@kcl.ac.uk>
2022-03-05 13:04:43 +0000

    build-index created in capacity and vulnerability

--- R/capacity/disasters-emergencies/wales/build-index.R
+++ R/capacity/disasters-emergencies/wales/build-index.R
@@ -2,6 +2,9 @@
 library(tidyverse)
 library(psych)
 library(recipes)
+library(corrr)
+library(ggplot2)
+library(GGally)
 source("R/utils.R")
 
 # Load indicators
@@ -10,13 +13,14 @@ wales_capacity_indicators <-
         path = "data/capacity/disasters-emergencies/wales",
         key = "lad_code"
     )
-plot_histogram(wales_capacity_indicators)
+# plot_histogram(wales_capacity_indicators)
 
-# Scale indicator, high value = high capacity
+# 1. Scale indicator, high value = high capacity
+# 2. Avoid skewness
+# 3. Access_to_internet is still moderate skewed
 wales_capacity_scaled <-
     wales_capacity_indicators %>%
     mutate(
-        # avoid skewness, access_to_internet is still moderate skewed
         charities = log(charities),  # avoid skewness
         community_assets = sqrt(community_assets) * -1, # High value = high capacity and avoid skewness
         la_spending_power = sqrt(la_spending_power - min(la_spending_power) + 1) # avoid skewness
@@ -35,88 +39,133 @@ print("After normalised:")
 describe(wales_capacity_normlised)
 plot_histogram(wales_capacity_normlised)
 
-# PCA
-library(corrr)
-wales_capacity_normlised %>% 
+
+
+# Step1: Correlation check
+## Sadly, no strong correlation :(
+wales_capacity_normlised %>%
     select(where(is.numeric)) %>%
-    correlate() %>% 
+    correlate() %>%
     rearrange()
 
-# ***
+## Correlation plot
+ggpairs(select(wales_capacity_normlised, where(is.numeric))) + theme_bw()
+
+## For analysis check
+wales_capacity_normlised %>%
+    ggparcoord(columns = 2:5, groupColumn = 1)
+
+
+# ***********************************************
 # Correlation is not strong, maximun is 0.582 between charities and la_spending_power
-# ***
+# Assume we can use PCA...
+# ***********************************************
+
+# PCA
+wales_recipe <-
+  recipe(~., data = wales_capacity_normlised) %>%
+  update_role(lad_code, new_role = "id") %>%
+  step_pca(all_predictors(), id = "pca") %>%
+  prep()
+
+# Step2: indentify the number of latent facors
+pca_variation_stat <-
+  wales_recipe %>%
+    tidy(id = "pca", type = "variance") %>%
+    filter(
+      terms ==  "cumulative percent variance" |
+      terms  == "percent variance" |
+      terms  == "variance"
+    ) %>%
+    pivot_wider(names_from = terms, values_from = value) %>% 
+    select(-id) %>% 
+    rename(
+      percent_variance = `percent variance`,
+      cumulative_percent_variance = `cumulative percent variance`
+    )
+
+# Step3: rotation of factos
+pca_rotation <-
+  wales_recipe %>%
+  tidy(id = "pca") %>%
+  pivot_wider(names_from = terms, values_from = value) %>%
+  select(-id)
+
+# Squared factor loading (scaled to unity sum)
+squared_factor_loading <-
+  pca_rotation %>%
+  select(-component) %>%
+  mutate_all(function(x) (x**2) / sum(x**2))
+
+# Concat all pca statistical infomation together
+pca_stat <-
+  bind_cols(
+    squared_factor_loading, pca_variation_stat
+  )
 
 
-# wales_recipe <-
-#   recipe(~., data = wales_capacity_normlised) %>%
-#   update_role(lad_code, new_role = "id") %>% 
-#   step_naomit(all_predictors()) %>% 
-#   step_normalize(all_predictors()) %>%
-#   step_pca(all_predictors(), id = "pca") %>% 
-#   prep()
+# calculate the weight of each variable
+pca_weight <-
+  pca_stat %>%
+  slice_head(n = 2) %>%  # only keep 2 components
+  mutate(
+    percent_variance = percent_variance / 100,  # convert to decimal
+    access_to_internet = access_to_internet * percent_variance,
+    charities = charities * percent_variance,
+    community_assets = community_assets * percent_variance,
+    la_spending_power = la_spending_power * percent_variance
+  ) %>%
+  select(access_to_internet, charities, community_assets, la_spending_power) %>% 
+  summarise(across(where(is.numeric), ~ sum(.x, na.rm = TRUE)))
+  
+tt = as_tibble(cbind(nms = names(pca_weight), t(pca_weight)))
 
-# wales_pca <- 
-#   wales_recipe %>% 
-#   tidy(id = "pca") 
+output <-
+  tt %>%
+  mutate(
+    weight = as.numeric(V2)
+  ) %>% 
+  select(-V2)
 
-# wales_capacity_normlised %>% 
-#   select(where(is.numeric)) %>% 
-#   drop_na() %>% 
-#   scale() %>% 
-#   prcomp() %>%  
-#   .$rotation
-
-# # Examine how much variance each component accounts for
-# wales_recipe %>% 
-#   tidy(id = "pca", type = "variance") %>% 
-#   filter(terms == "percent variance") %>% 
-#   ggplot(aes(x = component, y = value)) + 
-#   geom_col(fill = "#b6dfe2") + 
-#   xlim(c(0, 5)) + 
-#   ylab("% of total variance")
-
-# # plot loadings by principal component
-# library(ggplot2)
-# library(tidytext)
-# wales_pca %>%
-#   mutate(terms = reorder_within(terms, abs(value), component)) %>%
-#   ggplot(aes(abs(value), terms, fill = value > 0)) +
-#   geom_col() +
-#   facet_wrap(~component, scales = "free_y") +
-#   scale_y_reordered() +
-#   scale_fill_manual(values = c("#b6dfe2", "#0A537D")) +
-#   labs(
-#     x = "Absolute value of contribution",
-#     y = NULL, fill = "Positive?"
-#   )
-
-# # get pca loadings into wider format
-# wales_wider <- wales_pca %>%
-#   pivot_wider(names_from = component, id_cols = terms)
-
-# # define arrow style
-# arrow_style <- arrow(length = unit(.05, "inches"),
-#                      type = "closed")
+write_csv(
+  output,
+  "data/capacity/disasters-emergencies/wales/index-weighted.csv"
+)
 
 
-# # Plot PCA loadings + scores
-# pca_plot <-
-#   juice(wales_recipe) %>%
-#   ggplot(aes(PC1, PC2)) +
-#   geom_point(aes(color = lad_code, shape = lad_code), 
-#              alpha = 0.8, 
-#              size = 2) +
-#   scale_colour_manual(values = c("darkorange","purple","cyan4")) 
 
-# pca_plot +
-#   geom_segment(data = wales_wider,
-#                aes(xend = PC1, yend = PC2), 
-#                x = 0, 
-#                y = 0, 
-#                arrow = arrow_style) + 
-#   geom_text(data = wales_wider,
-#             aes(x = PC1, y = PC2, label = terms), 
-#             hjust = 0, 
-#             vjust = 1,
-#             size = 5, 
-#             color = '#0A537D') 
+# Some plots
+
+# 1.Examine how much variance each component accounts for
+wales_recipe %>% 
+  tidy(id = "pca", type = "variance") %>% 
+  filter(terms == "percent variance") %>% 
+  ggplot(aes(x = component, y = value)) + 
+  geom_col(fill = "#b6dfe2") + 
+  xlim(c(0, 5)) + 
+  ylab("% of total variance")
+
+# Examine how much cumulative variance each component accounts for
+wales_recipe %>% 
+  tidy(id = "pca", type = "variance") %>% 
+  filter(terms == "cumulative percent variance") %>% 
+  ggplot(aes(x = component, y = value)) + 
+  geom_col(fill = "#b6dfe2") + 
+  xlim(c(0, 5)) + 
+  ylab("cumulative percent variance")
+
+# plot loadings by principal component
+library(ggplot2)
+library(tidytext)
+wales_recipe %>%
+  tidy(id = "pca") %>% 
+  mutate(terms = reorder_within(terms, abs(value), component)) %>%
+  ggplot(aes(abs(value), terms, fill = value > 0)) +
+  geom_col() +
+  facet_wrap(~component, scales = "free_y") +
+  scale_y_reordered() +
+  scale_fill_manual(values = c("#b6dfe2", "#0A537D")) +
+  labs(
+    x = "Absolute value of contribution",
+    y = NULL, fill = "Positive?"
+  )
