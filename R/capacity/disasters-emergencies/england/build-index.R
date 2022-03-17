44156501a913cc21901e9380db1a0c90a6f5f181
Aileen McDonald <aileen_mcdonald14@hotmail.com>
2022-03-17 12:04:34 +0000

    looking into difference in ranks without fire response times

--- R/capacity/disasters-emergencies/england/build-index.R
+++ R/capacity/disasters-emergencies/england/build-index.R
@@ -2,7 +2,7 @@
 library(tidyverse)
 library(demographr)
 
-source("R/utils.R")
+source("https://raw.githubusercontent.com/britishredcrosssociety/resilience-index/main/R/utils.R")
 
 # ---- Load indicators ----
 indicators <-
@@ -36,6 +36,12 @@ indicators_aligned <- indicators_complete |>
   mutate(cps_millions = cps_millions * -1)
 
 # ---- Build Index ----
+
+# Check distribution
+indicators_aligned |>
+  normalise_indicators() |> 
+  summary()
+
 de <-
   indicators_aligned |>
   normalise_indicators() |>
@@ -45,4 +51,42 @@ de <-
 de_invert <- de |>
   mutate(de_domain_rank = inverse_rank(de_domain_rank),
          de_domain_quantiles = invert_this(de_domain_quantiles))
+
+
+# Check removing fire response time since at FRA level ----
+de_no_fire <-
+  indicators_aligned |>
+  select(-station_response_time) |>
+  normalise_indicators() |>
+  calculate_domain_scores(domain_name = "de") |>
+  select(lad_code, de_domain_rank_no_fire = de_domain_rank, de_domain_quantiles_no_fire = de_domain_quantiles)
+
+de_no_fire_change <- de |>
+  select(-de_domain_score) |> 
+  left_join(de_no_fire, by = "lad_code") |>
+  mutate(rank_change = de_domain_rank_no_fire - de_domain_rank,
+         quantile_change = de_domain_quantiles_no_fire - de_domain_quantiles) |> 
+  select(lad_code, rank_change, quantile_change)
+
+de_no_fire_change |> 
+  summary()
+
+de_no_fire_change |> 
+  group_by(quantile_change) |>
+  summarise(prop_of_lads = n()/nrow(de_no_fire_change))
+
+de_no_fire_change |> 
+  group_by(rank_change) |>
+  summarise(prop_of_lads = n()/nrow(de_no_fire_change))
+
+indicators_aligned |> 
+  select(-lad_code) |>
+  ggpairs()
+
+indicators_aligned |>
+  normalise_indicators() |>
+  select(-lad_code) |>
+  ggpairs()
+
+
 
