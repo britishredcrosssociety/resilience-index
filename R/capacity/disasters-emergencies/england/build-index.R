850a2addde39daae4eaa9b20c2d6acec9dc160ea
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-03-18 14:04:36 +0100

    Comment checks

--- R/capacity/disasters-emergencies/england/build-index.R
+++ R/capacity/disasters-emergencies/england/build-index.R
@@ -14,21 +14,21 @@ indicators <-
     key = "lad_code"
   )
 
-# Check entry for all 2021 LAD codes
-lads_21 <- lookup_lad_counties_21 |>
-  filter(str_detect(lad_21_code, "^E")) |>
-  distinct(lad_code = lad_21_code) 
+# # Check entry for all 2021 LAD codes
+# lads_21 <- lookup_lad_counties_21 |>
+#   filter(str_detect(lad_21_code, "^E")) |>
+#   distinct(lad_code = lad_21_code) 
 
-lads_21 |>
-  anti_join(indicators, by = "lad_code")
+# lads_21 |>
+#   anti_join(indicators, by = "lad_code")
 
-indicators |>
-  anti_join(lads_21, by = "lad_code")
+# indicators |>
+#   anti_join(lads_21, by = "lad_code")
 
-indicators |>
-  dplyr::filter(if_any(everything(), ~is.na(.x))) 
-# City of London & Isle of Scilly are both missing civic assests & engagement
-# Assumption: drop these LADs due to too many missing variables. 
+# indicators |>
+#   dplyr::filter(if_any(everything(), ~is.na(.x))) 
+# # City of London & Isle of Scilly are both missing civic assests & engagement
+# # Assumption: drop these LADs due to too many missing variables. 
 
 indicators_complete <- indicators |>
   drop_na()
