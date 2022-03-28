f62e09fa24b3a5498c388ae33a14db00bf0f9a52
Aileen McDonald <aileen_mcdonald14@hotmail.com>
2022-03-28 11:37:04 +0100

    2019 LA spending and compare 2021 and 2019 deciles and ranks

--- R/capacity/disasters-emergencies/england/build-index.R
+++ R/capacity/disasters-emergencies/england/build-index.R
@@ -14,19 +14,19 @@ indicators <-
     key = "lad_code"
   )
 
-# # Check entry for all 2021 LAD codes
-# lads_21 <- lookup_lad_counties_21 |>
-#   filter(str_detect(lad_21_code, "^E")) |>
-#   distinct(lad_code = lad_21_code) 
-
-# lads_21 |>
+# Check entry for all 2019 LAD codes
+# lads_19 <- lookup_lad_counties_19 |>
+#   filter(str_detect(lad_19_code, "^E")) |>
+#   distinct(lad_code = lad_19_code)
+# 
+# lads_19 |>
 #   anti_join(indicators, by = "lad_code")
-
+# 
 # indicators |>
-#   anti_join(lads_21, by = "lad_code")
-
+#   anti_join(lads_19, by = "lad_code")
+# 
 # indicators |>
-#   dplyr::filter(if_any(everything(), ~is.na(.x))) 
+#   dplyr::filter(if_any(everything(), ~is.na(.x)))
 # # City of London & Isle of Scilly are both missing civic assests & engagement
 # # Assumption: drop these LADs due to too many missing variables. 
 
@@ -54,7 +54,52 @@ de_invert <- de |>
   mutate(de_domain_rank = inverse_rank(de_domain_rank),
          de_domain_quantiles = invert_this(de_domain_quantiles))
 
+# Compare to 2021 LAD calcs ----
+de_2021 <- read_csv("https://raw.githubusercontent.com/britishredcrosssociety/resilience-index/96183f7994c36f875318cffcd1847245663e29e8/data/capacity/disasters-emergencies/england/de-index.csv")
 
+de_2021 <- de_2021 |>
+  rename_with(~paste0(., "_21"), where(is.numeric))
+
+de_2019 <- de_invert |>
+  rename_with(~paste0(., "_19"), where(is.numeric))
+
+# Only compare LADs in both years
+de_compare <- de_2019 |> 
+  inner_join(de_2021, by = "lad_code") |>
+  select(lad_code, de_domain_rank_19, de_domain_quantiles_19, de_domain_rank_21, de_domain_quantiles_21) |>
+  mutate(rank_change = de_domain_rank_21 - de_domain_rank_19,
+         quintiles_change = de_domain_quantiles_21 - de_domain_quantiles_19)
+
+de_compare |> 
+  group_by(rank_change) |>
+  summarise(count = n(), 
+            prop = n() / nrow(de_compare))
+
+de_compare |> 
+  group_by(quintiles_change) |>
+  summarise(count = n(), 
+            prop = n() / nrow(de_compare))
+
+de_2019 |> 
+  anti_join(de_2021, by = "lad_code")
+
+de_2021 |> 
+  anti_join(de_2019, by = "lad_code")
+
+# Build index for VCSEP Risk Indicator Tool (Quintiles not Deciles) -----
+de_quint <- indicators_aligned |>
+  normalise_indicators() |>
+  calculate_domain_scores(domain_name = "de", num_quantiles = 5) 
+
+# Inverting ranks and deciles so that higher scores = higher capacity
+de_quint_invert <- de_quint |>
+  mutate(de_domain_rank = inverse_rank(de_domain_rank),
+         de_domain_quantiles = invert_this(de_domain_quantiles))
+
+# Save 
+de_quint_invert |>
+  select(lad_code, de_domain_quantiles) |>
+  write_csv("data/capacity/disasters-emergencies/england/de-index-quint.csv")
 
 
 
--- R/capacity/disasters-emergencies/england/la-spending-power.R
+++ R/capacity/disasters-emergencies/england/la-spending-power.R
@@ -3,6 +3,7 @@ library(geographr)
 library(sf)
 library(tidyverse)
 library(readxl)
+library(demographr)
 
 source("R/utils.R")
 
@@ -12,61 +13,44 @@ tf <- download_file("https://data.parliament.uk/resources/constituencystatistics
 
 raw <- read_excel(tf, sheet = "Spending power", skip = 3)
 
+# Take 2019/2020 Spending since at correct 2019 LAD codes 
 clean <- raw |>
   select(lad_code = `ONS code`, 
          lad_name = `Local authority`, 
          geo_type = `Local authority class`, 
-         cps_millions = `2021/22`)
+         cps_millions = `2019/20`)
 
 clean |>
   distinct(geo_type)
-# There are a mix og geographic types in the data so will take each in turn
+# There are a mix of geographic types in the data so will take each in turn
+
+# Check includes all 2019 LADS
+lads_2019 <- lookup_lad_19_counties_ua_19 |> 
+  distinct(lad_19_code) |>
+  filter(str_detect(lad_19_code, "^E"))
+
+lads_2019 |>
+  anti_join(clean, by = c("lad_19_code" = "lad_code"))
 
 # LTLA -----
 
 # Load in population data
-lad_pop <-
-  population_lad |>
+lad_pop_update <-
+  population_lad_19_codes_19 |>
   select(lad_code, 
          pop = total_population) |>
   filter(str_detect(lad_code, "^E"))
 
-# Check if using 2021 LAD codes
-if (
-  anti_join(
-    lad_pop,
-    lookup_lad_over_time,
-    by = c("lad_code" = "LAD21CD")
-  ) |>
-    pull(lad_code) |>
-    length() != 0
-) {
-  stop("Lad codes need changing to 2021 - check if 2019 or 2020")
-}
-
-# Update population from 2020 to 2021
-# Aggregation only of LADs between 2019 to 2021
-
-# Distinct table foe 2020 - 2021 due to E06000060
-lookup_lad_over_time_2020 <- lookup_lad_over_time |>
-  distinct(LAD20CD, LAD21CD)
-
-lad_pop_update <- lad_pop |>
-  left_join(lookup_lad_over_time_2020, by = c("lad_code" = "LAD20CD")) |>
-  group_by(LAD21CD) |>
-  summarise(across(where(is.numeric), sum))
-
 ltla_cps <- lad_pop_update |>
-  left_join(clean, by = c("LAD21CD" = "lad_code")) |>
-  select(LAD21CD, cps_millions)
+  left_join(clean, by = "lad_code")
 
 # Check any LTLAs not in the data
 lad_pop_update |>
-  anti_join(clean, by = c("LAD21CD" = "lad_code"))
+  anti_join(clean, by = "lad_code")
 # None missing
 
 remaining_ltla <- clean |>
-  anti_join(lad_pop_update, by = c("lad_code" = "LAD21CD"))
+  anti_join(lad_pop_update, by = "lad_code")
 
 # Fire & Rescue Authorities ----
 fire_cps <- remaining_ltla |>
@@ -74,16 +58,16 @@ fire_cps <- remaining_ltla |>
   rename(fra_code = lad_code, 
          fra_name = lad_name)
 
-# FRA to LAD lookup data
-# Source: https://geoportal.statistics.gov.uk/datasets/ons::local-authority-district-to-fire-and-rescue-authority-april-2021-lookup-in-england-and-wales/about
-response <- httr::GET("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/LAD21_FRA21_EW_LU/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json")
+# FRA to LAD lookup data 2019
+# Source: https://geoportal.statistics.gov.uk/datasets/ons::local-authority-district-to-fire-and-rescue-authority-december-2019-lookup-in-england-and-wales/about
+response <- httr::GET("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/LAD19_FRA19_EW_LU/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json")
 content <- httr::content(response, type = "application/json", simplifyVector = TRUE)
 
 fra_lad_lookup <- content$features$attributes |>
-  select(ltla_code = LAD21CD, 
-         ltla_name = LAD21NM, 
-         fra_code = FRA21CD, 
-         fra_name = FRA21NM) |>
+  select(ltla_code = LAD19CD, 
+         ltla_name = LAD19NM, 
+         fra_code = FRA19CD, 
+         fra_name = FRA19NM) |>
   filter(!str_detect(fra_name, "Wales"))
 
 # Check if all FRS in the data
@@ -100,11 +84,11 @@ fra_lad_lookup |>
 
 ltla_fire_cps <- fire_cps |>
   inner_join(fra_lad_lookup, by = "fra_code") |>
-  left_join(lad_pop_update, by = c("ltla_code" = "LAD21CD")) |>
+  left_join(lad_pop_update, by = c("ltla_code" = "lad_code")) |>
   group_by(fra_code) |>
   mutate(pop_weighted_cps = pop / sum(pop) * cps_millions) |>
   ungroup() |>
-  select(LAD21CD = ltla_code, 
+  select(lad_code = ltla_code, 
          cps_millions = pop_weighted_cps)
 
 remaining_fire <- remaining_ltla |>
@@ -112,7 +96,7 @@ remaining_fire <- remaining_ltla |>
 
 # UTLAs -----
 
-utla_pop <- population_counties_ua |>
+utla_pop <- population_counties_ua_19_codes_19 |>
   select(county_ua_code, pop = total_population)
 
 utla_cps <- remaining_fire |>
@@ -130,25 +114,13 @@ utla_cps <- remaining_fire |>
 # it just depends on the area whether the responsibility is funded via the LTLA or UTLA. 
 
 ltla_utla_cps <- utla_cps |>
-  left_join(lookup_counties_ua_lad, by = "county_ua_code") |>
-  left_join(lad_pop_update, by = c("lad_code" = "LAD21CD")) |>
+  left_join(lookup_lad_19_counties_ua_19, by = c("county_ua_code" = "county_ua_19_code")) |>
+  left_join(lad_pop_update, by = c("lad_19_code" = "lad_code")) |>
   group_by(county_ua_code) |>
   mutate(pop_weighted_cps = ifelse(cps_millions == 0, 0, pop / sum(pop) * cps_millions)) |>
   ungroup() |>
-  select(lad_code, 
+  select(lad_code = lad_19_code, 
          cps_millions = pop_weighted_cps)
-
-# Update to 2021 LAD codes
-ltla_utla_cps_updated <- ltla_utla_cps |>
-  left_join(lookup_lad_over_time_2020, by = c("lad_code" = "LAD20CD")) |>
-  group_by(LAD21CD) |>
-  summarise(across(where(is.numeric), sum))
-
-# Check if these LTLAs from splitting UTLAs are already in the data as LTLAs
-ltla_utla_cps_updated |>
-  inner_join(ltla_cps, by = "LAD21CD")
-# Yes they are already in the LTLA level data so this spending power is in addition
-# to the LTLA level spending power
 
 remaining_utla <- remaining_fire |>
   anti_join(utla_cps, by = c("lad_code" = "county_ua_code"))
@@ -156,8 +128,8 @@ remaining_utla <- remaining_fire |>
 # except 'Greater Manchester Combined Authority' and 'Greater London Authority
 
 # Combined Authority ----
-# Combined Authority lookup source: https://geoportal.statistics.gov.uk/datasets/ons::local-authority-district-to-combined-authority-december-2020-lookup-in-england/about
-response <- httr::GET("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/LAD20_CAUTH20_EN_LU/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json")
+# Combined Authority lookup source: https://geoportal.statistics.gov.uk/datasets/ons::local-authority-district-to-combined-authority-december-2019-lookup-in-england/about
+response <- httr::GET("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/LAD19_CAUTH19_EN_LU/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json")
 content <- httr::content(response, type = "application/json", simplifyVector = TRUE)
 
 combined_auth_lookup <- content$features$attributes |>
@@ -166,15 +138,15 @@ combined_auth_lookup <- content$features$attributes |>
 
 # There are no changes between 2020 and 2021 LAD codes for Greater Manchester LTLAs
 combined_auth_ltla_cps <- remaining_utla |>
-  inner_join(combined_auth_lookup, by = c("lad_code" = "CAUTH20CD")) |>
-  left_join(lad_pop_update, by = c("LAD20CD" = "LAD21CD")) |>
+  inner_join(combined_auth_lookup, by = c("lad_code" = "CAUTH19CD")) |>
+  left_join(lad_pop_update, by = c("LAD19CD" = "lad_code")) |>
   mutate(pop_weighted_cps = pop / sum(pop) * cps_millions) |>
-  select(LAD21CD = LAD20CD, 
+  select(lad_code = LAD19CD, 
          cps_millions)
 
 # Check if these LTLAs from splitting UTLAs are already in the data as LTLAs
 combined_auth_ltla_cps |>
-  inner_join(ltla_cps, by = "LAD21CD")
+  inner_join(ltla_cps, by = "lad_code")
 # Yes they are already in the LTLA level data so this spending power is in addition
 # to the LTLA level spending power
 
@@ -194,7 +166,7 @@ gla_lad_codes <- gla_clean |>
   filter(!is.na(inner_outer))
 
 gla_lad_codes |>
-  anti_join(lad_pop_update, by = c("lad_code" = "LAD21CD"))
+  anti_join(lad_pop_update, by = "lad_code")
 
 gla_cps_value <- remaining_utla |>
   filter(lad_name == "Greater London Authority") |>
@@ -202,24 +174,24 @@ gla_cps_value <- remaining_utla |>
 
 gla_ltla_cps <- gla_lad_codes |>
   mutate(cps_millions = gla_cps_value) |>
-  left_join(lad_pop_update, by = c("lad_code" = "LAD21CD")) |>
+  left_join(lad_pop_update, by = "lad_code") |>
   mutate(pop_weighted_cps = pop / sum(pop) * cps_millions) |>
-  select(LAD21CD = lad_code, 
+  select(lad_code, 
          cps_millions = pop_weighted_cps)
 
 # Check if these LTLAs from splitting GLA are already in the data as LTLAs
 gla_ltla_cps |>
-  inner_join(ltla_cps, by = "LAD21CD")
+  inner_join(ltla_cps, by = "lad_code")
 # Yes they are already in the LTLA level data so this spending power is in addition
 # to the LTLA level spending power
 
 # Combining all the data sets ---
 combined <- ltla_cps |>
   bind_rows(ltla_fire_cps) |>
-  bind_rows(ltla_utla_cps_updated) |>
+  bind_rows(ltla_utla_cps) |>
   bind_rows(combined_auth_ltla_cps) |>
   bind_rows(gla_ltla_cps) |>
-  group_by(lad_code = LAD21CD) |>
+  group_by(lad_code) |>
   summarise(cps_millions = sum(cps_millions))
 
 # Save data ----
