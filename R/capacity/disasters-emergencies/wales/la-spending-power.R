e91f21df7aa5805a97ad488c211945acbb6b0fc1
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-04-08 16:08:06 +0200

    Recalculate spending power using group sums

--- R/capacity/disasters-emergencies/wales/la-spending-power.R
+++ R/capacity/disasters-emergencies/wales/la-spending-power.R
@@ -7,44 +7,25 @@ source("R/utils.R")
 
 # Download data
 # Source: https://statswales.gov.wales/Catalogue/Local-Government/Finance/Revenue/Budgets/budgetedrevenueexpenditure-by-authority-service
-df <- statswales_get_dataset("lgfs0030")
+df <-
+    statswales_get_dataset("lgfs0030") |>
+    as_tibble()
 
 # Clean data
-df_clean <-
-    df %>%
+df_sum <-
+    df |>
     filter(
-        # Use data 2021-2022
-        Year_ItemName_ENG == "2021-22" &
-        # Two measure: ￡ thousand; ￡ per head. We use ￡ thousand
-        str_detect(Measure_ItemName_ENG, pattern = "thousand") &
-        # Only select local authority
+        Year_ItemName_ENG == "2021-22",
+        str_detect(Measure_ItemName_ENG, pattern = "per head"),
         str_detect(Authority_AltCode1, "^W060000")
-    ) %>%
+    ) |>
     select(
         lad_code = Authority_AltCode1,
         variable = Description_ItemName_ENG,
         value = Data
-    ) %>%
-    group_by(variable, lad_code) %>%  # This is preparation for pivot table
-    mutate(
-        row = row_number()  # This is preparation for pivot table
-    ) %>%
-    pivot_wider(
-        names_from = variable,
-        values_from = value
-    ) %>%
-    select(-row) %>%  # Drop unnecessary column
-    group_by(lad_code) %>%
-    summarise(across(where(is.numeric), ~ sum(.x, na.rm = TRUE)))  # ???for each LA, add up in the same column
-
-df_sum <-
-    df_clean %>%
-    mutate(
-        across(where(is.numeric), normalise),  # normalise each column
-        sum = rowSums(across(where(is.numeric)),  na.rm = TRUE)  # Calculate a sum column
-    ) %>%
-    select(lad_code, sum) %>%
-    rename(la_spending_power = sum)
+    ) |>
+    group_by(lad_code) |>
+    summarise(la_spending_power = sum(value))
 
 write_rds(
     df_sum,
