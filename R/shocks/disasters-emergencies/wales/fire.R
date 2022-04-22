14a17aff2195626a547a9f5d928b3da5f3258164
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-04-22 15:01:09 +0200

    Filter to domestic fires and normalise by population

--- R/shocks/disasters-emergencies/wales/fire.R
+++ R/shocks/disasters-emergencies/wales/fire.R
@@ -1,26 +1,45 @@
+# ---- Load libs ----
 library(tidyverse)
 library(statswalesr)
-library(stringr)
+library(demographr)
 
+# ---- Source data ----
 # Download data
 # Source: https://statswales.gov.wales/Catalogue/Community-Safety-and-Social-Inclusion/Community-Safety/Fire-Incidents/Fires-and-False-Alarms/fires-by-localauthority-financialyear-motive
-df <- statswales_get_dataset("CSAF0066")
+raw <-
+    statswales_get_dataset("CSAF0066") |>
+    as_tibble()
 
-df_clean <-
-    df %>%
+# ---- Clean ----
+# Filter to latest fires from all motives
+num_fires <-
+    raw |>
     filter(
-        str_detect(LocalAuthority_AltCode1, "^W06") &
-        Year_ItemName_ENG == "2020-21" &
-        Typeoffires_ItemName_ENG == "All Fires" &
-        Grasslandmarker_ItemName_ENG == "All fires" &
-        Motive_ItemName_ENG == "All Motives"
-    ) %>%
+        Year_ItemName_ENG == "2020-21",
+        str_detect(LocalAuthority_AltCode1, "^W06"),
+        Typeoffires_ItemName_ENG != "Chimney Fires",
+        Typeoffires_ItemName_ENG != "All Fires",
+        Motive_ItemName_ENG == "All Motives",
+        Grasslandmarker_ItemName_ENG == "Not grassland, woodland or crop fires"
+    ) |>
     select(
         lad_code = LocalAuthority_AltCode1,
-        value = Data
-    )
+        value = Data,
+        Typeoffires_ItemName_ENG,
+        Motive_ItemName_ENG,
+        Grasslandmarker_ItemName_ENG
+    ) |>
+    group_by(lad_code) |>
+    summarise(num_fires = sum(value))
 
+num_fires_normalised <-
+    num_fires |>
+    left_join(population_lad_20_codes_21, by = c("lad_code" = "lad_21_code")) |>
+    mutate(num_fires_per_1000 = num_fires / total_population * 1000) |>
+    select(lad_code, num_fires_per_1000)
+
+# ---- Save ----
 write_rds(
-    df_clean,
+    num_fires_normalised,
     "data/shocks/disasters-emergencies/wales/fire-lad.rds"
 )
