ca10b4ec718a5b433faf7e8127ddb8f1ded4d5df
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-04-22 15:55:05 +0200

    Normalise flood risk by pop

--- R/shocks/disasters-emergencies/wales/flood.R
+++ R/shocks/disasters-emergencies/wales/flood.R
@@ -1,38 +1,51 @@
+# ---- Load libs ----
 library(tidyverse)
 library(statswalesr)
-library(stringr)
+library(demographr)
 
+# ---- Load data ----
 # Download data
+# Properties at risk of flooding
 # Source: https://statswales.gov.wales/Catalogue/Environment-and-Countryside/Flooding/environment-and-countryside-state-of-the-environment-our-local-environment-properties-at-risk-of-flooding
-df <- statswales_get_dataset("ENVI0211")
+raw <-
+    statswales_get_dataset("ENVI0211") |>
+    as_tibble()
 
+# ---- Clean ----
+# Calculate the number of properties at risk of any type of flooding, by any
+# level of risk
+
+# ---- Variable Guide: --------------------------------------------------------
 # FloodingType_Code: River, Surface, Tidal
-df_clean <-
-    df %>%
+# Risk_ItemName_ENG: High, High & Medium, Low, Medium, Total
+# -----------------------------------------------------------------------------
+num_properties <-
+    raw |>
     filter(
         str_detect(Area_Code, "^W06") &
-        Risk_ItemName_ENG == "Total"
-    ) %>%
+            Risk_ItemName_ENG == "Total"
+    ) |>
     select(
         lad_code = Area_Code,
-        variable = FloodingType_Code,
-        value = Data
-    ) %>%
-    group_by(variable, lad_code) %>%  # This is preparation for pivot table
-    mutate(
-        row = row_number()  # This is preparation for pivot table
-    ) %>%
-    pivot_wider(
-        names_from = variable,
-        values_from = value
-    ) %>%
-    mutate(
-        total = as.numeric(River) + as.numeric(Surface) + as.numeric(Tidal)
-    ) %>%
-    select(lad_code, total)
+        flood_type = FloodingType_Code,
+        num_properties = Data
+    ) |>
+    mutate(num_properties = as.integer(num_properties)) |>
+    group_by(lad_code) |>
+    summarise(total_num_properties = sum(num_properties))
 
-# more flood, more deprived
+flood_risk <-
+    num_properties |>
+    left_join(population_lad_20_codes_21, by = c("lad_code" = "lad_21_code")) |>
+    mutate(
+        properties_flood_risk_per_1000 = total_num_properties / total_population * 1000
+    ) |>
+    select(lad_code, properties_flood_risk_per_1000)
+
+
+
+# ---- Save ----
 write_rds(
-    df_clean,
+    flood_risk,
     "data/shocks/disasters-emergencies/wales/flood-lad.rds"
 )
