3774a7eba5074c2d19d43bd52312d245d63c4faf
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-04-22 16:53:01 +0200

    Refactor code

--- R/capacity/disasters-emergencies/wales/la-spending-power.R
+++ R/capacity/disasters-emergencies/wales/la-spending-power.R
@@ -1,19 +1,18 @@
+# ---- Load ----
 library(tidyverse)
-library(dplyr)
 library(statswalesr)
-library(stringr)
 library(geographr)
 source("R/utils.R")
 
 # Download data
 # Source: https://statswales.gov.wales/Catalogue/Local-Government/Finance/Revenue/Budgets/budgetedrevenueexpenditure-by-authority-service
-df <-
+raw <-
     statswales_get_dataset("lgfs0030") |>
     as_tibble()
 
-# Clean data
-df_sum <-
-    df |>
+# ---- Clean ----
+expenditure <-
+    raw |>
     filter(
         Year_ItemName_ENG == "2021-22",
         str_detect(Measure_ItemName_ENG, pattern = "per head"),
@@ -28,6 +27,6 @@ df_sum <-
     summarise(la_spending_power = sum(value))
 
 write_rds(
-    df_sum,
+    expenditure,
     "data/capacity/disasters-emergencies/wales/la-spending-power-lad.rds"
 )
