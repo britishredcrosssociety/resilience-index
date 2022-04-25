468451e365b0a3590c93d1aff23cb2de581cada0
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-04-25 17:36:25 +0200

    Update to version < 1.0 of geographr

--- R/capacity/disasters-emergencies/wales/community-assets.R
+++ R/capacity/disasters-emergencies/wales/community-assets.R
@@ -2,30 +2,55 @@
 library(tidyverse)
 library(readxl)
 library(geographr)
+library(demographr)
 source("R/utils.R")
 
-community_assets_raw <- read_excel("data/on-disk/community-needs-index/community-needs-index/WCNI and domains - 2021.xlsx")
+# OSCI Community Needs Index data ----
+# Prepare the Community Needs Index, showing the Index and its domains for all
+# Wards, along with which are the left-behind areas
+# More info: https://ocsi.uk/2019/10/21/community-needs-index-measuring-social-and-cultural-factors/
+# The data isn't public but OCSI can provide it to social purpose organisations
+# - email them for info
 
-# The bigger number of 'Civic_Assets_Domain', the more deprive is
-community_assets_lad <-
-  community_assets_raw %>%
+# Higher score and therefore lower rank is the LACK of assets so HIGH SCORE/LOW
+# RANK = LOW CAPABILITIES
+# Civic assets + Engagement + Connectness = Community Need score (high score =
+# high need)
+
+# Data is at 2017 ward level
+raw <-
+  read_excel(
+    "data/on-disk/OCSI/community-needs-index/WCNI and domains - 2021.xlsx"
+  )
+
+civic_assets_vars <-
+  raw |>
   select(
-    msoa_code = "MSOA code",
-    Civic_Assets_Domain = "Civic_Assets_Domain"
-  ) %>%
-  left_join(lookup_msoa_lad, by = "msoa_code") %>%
-  left_join(population_msoa, by = "msoa_code") %>%
+    msoa11_code = `MSOA code`,
+    civic_assets_score = Civic_Assets_Domain
+  )
 
+civic_assets_joined <-
+  population_msoa_20_codes_11 |>
+  select(msoa11_code = msoa_11_code, pop = total_population) |>
+  right_join(civic_assets_vars) |>
+  left_join(lookup_msoa11_ltla21) |>
+  select(-ends_with("_name"))
+
+civic_assets_ltla <-
+  civic_assets_joined |>
   calculate_extent(
-    var = Civic_Assets_Domain,
-    higher_level_geography = lad_code,
-    population = total_population,
+    var = civic_assets_score,
+    higher_level_geography = ltla21_code,
+    population = pop,
     weight_high_scores = TRUE
-  ) %>%
-  rename(community_assets = extent)
-
+  ) |>
+  rename(
+    lad_code = ltla21_code,
+    community_assets = extent
+  )
 
 write_rds(
-  community_assets_lad,
+  civic_assets_ltla,
   "data/capacity/disasters-emergencies/wales/community_assets_lad.rds"
 )
